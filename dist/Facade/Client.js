12e514257ee99553310774933fbdfebce818f8c9
zhaoyuanbo <zhaoyuanbo@dobest.com>
2022-03-28 18:05:20 +0800

    feat:描述

--- dist/Facade/Client.js
+++ dist/Facade/Client.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Client = void 0;
 const Facade_1 = require("./Facade");
+/**
+ * 外观模式通过定义一个一致的接口，用以屏蔽内部子系统的细节，使得调用端
+ * 只需跟这个接口发生调用，而无需关心这个子系统的内部细节
+ */
 class Client {
     static main() {
         const facade = new Facade_1.Facade();
